05c2baa4cb95b4263f3c08e94c0b4e1fb1495368
rxinwater <ryanaltjekiddo123@gmail.com>
2026-05-19 11:23:18 +0200

    Added script
    Bugged, but cant test further without proper player and player movement, postponed for now.

---
+++ JunkFoodSurvivors/Assets/Scripts/Enemy/EnemySpawn.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EnemySpawn : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public GameObject EnemyPrefab;
+    public Transform player;
+    public float minRadius = 3f;
+    public float maxRadius = 6f;
+    void Start()
+    {
+        
+    }
+
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnEnemy();
+    }
+
+    void SpawnEnemy()
+    {
+        Vector2 direction = Random.insideUnitCircle.normalized;
+        float distance = Random.Range(minRadius, maxRadius);
+        
+
+        Vector2 spawnPosition = (Vector2)player.position + direction * distance;
+
+       GameObject enemy = Instantiate(EnemyPrefab, spawnPosition, Quaternion.identity);
+    }
+
+
+
+}
