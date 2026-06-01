3b6bf85145a5a1736fa80dfa3ae184257de6f4c7
Ghost <144106974+SeanNeerings1@users.noreply.github.com>
2026-06-01 03:38:48 +0200

    Add TextMeshPro and XP UI
    Add TextMeshPro and XP UI

--- JunkFoodSurvivors/Assets/Scripts/Player/PlayerXp.cs
+++ JunkFoodSurvivors/Assets/Scripts/Player/PlayerXp.cs
@@ -1,63 +1,89 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
+using UnityEngine.InputSystem;
 
 public class PlayerXp : MonoBehaviour
 {
-    [Header("experience")]
-    [SerializeField] AnimationCurve experienceCurve;
-    
-    int currentLevel, totalexperience;
-    int previousLevelExperience, nextLevelExperience;
+    private int currentLevel;
+    private int totalExperience;
+    private int previousLevelExperience;
+    private int nextLevelExperience;
 
-    [Header("interface")]
-    [SerializeField] TextMeshProUGUI levelText;
-    [SerializeField] TextMeshProUGUI experienceText;
-    [SerializeField] Image experiencefill;
+    [Header("Interface")]
+    [SerializeField] private TextMeshProUGUI levelText;
+    [SerializeField] private TextMeshProUGUI experienceText;
+    [SerializeField] private Image experienceFill;
 
-
-    void Start()
+    private void Start()
     {
-        UpdateLevel();   
+        UpdateLevel();
+        UpdateInterface();
     }
 
-    void Update()
+    private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        // TESTING: Gain 5 XP when left-clicking
+        if (Mouse.current != null &&
+            Mouse.current.leftButton.wasPressedThisFrame)
         {
             AddExperience(5);
-        }   
+        }
     }
+
     public void AddExperience(int amount)
     {
-        totalexperience += amount;
+        totalExperience += amount;
+
+        Debug.Log("XP Added: " + amount);
+        Debug.Log("Total XP: " + totalExperience);
+
         CheckForLevelUp();
         UpdateInterface();
     }
 
-    void CheckForLevelUp()
+    private void CheckForLevelUp()
     {
-        while (totalexperience >= nextLevelExperience)
+        int safety = 0;
+
+        while (totalExperience >= nextLevelExperience && safety < 100)
         {
             currentLevel++;
             UpdateLevel();
+
+            Debug.Log("LEVEL UP! Level " + currentLevel);
+
+            safety++;
         }
     }
-    void UpdateLevel()
-    {
-        previousLevelExperience = (int)experienceCurve.Evaluate(currentLevel);
-        nextLevelExperience = (int)experienceCurve.Evaluate(currentLevel + 1);
-        UpdateInterface();
-    }
-    void UpdateInterface()
-    {
-        int start = totalexperience - previousLevelExperience;
-        int end = nextLevelExperience - previousLevelExperience;
 
-        levelText.text = currentLevel.ToString();
-        experienceText.text = start + "exp /" + end + " exp";
-        experiencefill.fillAmount = (float)start / (float)end;
+    private void UpdateLevel()
+    {
+        previousLevelExperience = GetRequiredXPForLevel(currentLevel);
+        nextLevelExperience = GetRequiredXPForLevel(currentLevel + 1);
+    }
+
+    private int GetRequiredXPForLevel(int level)
+    {
+        return level * level * 100;
+    }
+
+    private void UpdateInterface()
+    {
+        int currentLevelXP = totalExperience - previousLevelExperience;
+        int requiredXP = nextLevelExperience - previousLevelExperience;
+
+        if (levelText != null)
+            levelText.text = currentLevel.ToString();
+
+        if (experienceText != null)
+            experienceText.text = currentLevelXP + " XP / " + requiredXP + " XP";
+
+        if (experienceFill != null)
+        {
+            experienceFill.fillAmount = requiredXP > 0
+                ? (float)currentLevelXP / requiredXP
+                : 0f;
+        }
     }
 }
