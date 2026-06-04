0ecbe4fafd444a0c18068c8d0ad44ba4440f1b10
mikoreo <michael@de-ru.net>
2026-06-04 15:50:12 +0200

    Health bar implemented in test1

--- JunkFoodSurvivors/Assets/Scripts/Player/Health/PlayerHealthBarTest.cs
+++ JunkFoodSurvivors/Assets/Scripts/Player/Health/PlayerHealthBarTest.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
 public class PlayerHealthBarTest : MonoBehaviour
 {
     public int MaxHealth = 100;
     public int CurrentHealth;
 
     public HealthBar healthBar;
+
+    private bool raaktVijandAan = false;
+    private float damageCooldown = 1.0f;
+    private float lastDamageTime;
+
     void Start()
     {
         CurrentHealth = MaxHealth;
@@ -16,15 +20,37 @@ public class PlayerHealthBarTest : MonoBehaviour
 
     void Update()
     {
-        if (Keyboard.current != null && Keyboard.current.spaceKey.wasPressedThisFrame)
+        if (raaktVijandAan && Time.time >= lastDamageTime + damageCooldown)
         {
             TakeDamage(10);
+            lastDamageTime = Time.time;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            raaktVijandAan = true;
+            TakeDamage(10);        
+            lastDamageTime = Time.time;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            raaktVijandAan = false;
         }
     }
 
     void TakeDamage(int damage)
     {
         CurrentHealth -= damage;
+
+
+        if (CurrentHealth < 0) CurrentHealth = 0;
 
         healthBar.SetHealth(CurrentHealth);
     }
