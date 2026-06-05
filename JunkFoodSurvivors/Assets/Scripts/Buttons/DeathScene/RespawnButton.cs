c3d526e485c7714db3587e9b1d57f8ecadef3029
GSL-6ofh <guillianlangendonk@gmail.com>
2026-06-05 10:02:29 +0200

    SoundButtonMichaelStart

--- JunkFoodSurvivors/Assets/Scripts/Buttons/DeathScene/RespawnButton.cs
+++ JunkFoodSurvivors/Assets/Scripts/Buttons/DeathScene/RespawnButton.cs
@@ -1,10 +1,30 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections; // Nodig voor de Coroutine
 
 public class DeathMenu : MonoBehaviour
 {
+    //drag audio source here
+    public AudioSource buttonClickSound;
+
     public void Respawn()
     {
+        // starts routine for sound
+        StartCoroutine(PlaySoundAndLoad());
+    }
+
+    IEnumerator PlaySoundAndLoad()
+    {
+        // if there is sound play it
+        if (buttonClickSound != null)
+        {
+            buttonClickSound.Play();
+
+            //waits till sound is done before loading
+            yield return new WaitForSeconds(buttonClickSound.clip.length);
+        }
+
+        // loads new scene
         SceneManager.LoadScene("Test 1");
     }
 }
